070e9a8ceca48350a9fe2673670e741a2875042b
Frederiknoer <Frederikk@live.dk>
2019-11-12 15:56:39 +0100

    Code clean-up

--- Arduino/rx-tx/rx-tx.ino
+++ Arduino/rx-tx/rx-tx.ino
@@ -1,5 +1,6 @@
 #include <SPI.h>
 #include <DW1000.h>
+#include "DW1000Ranging.h"
 
 // connection pins
 const uint8_t PIN_RST = 9; // reset pin
@@ -10,7 +11,7 @@ const uint8_t PIN_SS = SS; // spi select pin
 volatile boolean received = false;
 volatile boolean error = false;
 volatile int16_t numReceived = 0; // todo check int type
-String message;
+byte message[6];     //  [ ID_1, ID_2, FLOAT[0], FLOAT[1], FLOAT[2], FLOAT[3] ]
 
 // DEBUG packet sent status and count **** FROM TRANSMITTER ****
 boolean sent = false;
@@ -22,8 +23,9 @@ DW1000Time sentTime;
 
 void setup() 
 {
-  //Identical for both
   Serial.begin(9600);
+
+  //init the configuration
   DW1000.begin(PIN_IRQ, PIN_RST);
   DW1000.select(PIN_SS);
   DW1000.newConfiguration();
@@ -44,6 +46,13 @@ void setup()
   DW1000.attachSentHandler(handleSent);
   transmitter();
 
+}
+
+// *********** RANGING FUNCTIONS ****************
+void newRange() {
+  Serial.print("from: "); Serial.print(DW1000Ranging.getDistantDevice()->getShortAddress(), HEX);
+  Serial.print("\t Range: "); Serial.print(DW1000Ranging.getDistantDevice()->getRange()); Serial.print(" m");
+  Serial.print("\t RX power: "); Serial.print(DW1000Ranging.getDistantDevice()->getRXPower()); Serial.println(" dBm");
 }
 
 // **********  RECEIVER FUCNTIONS ***************
@@ -74,8 +83,8 @@ void transmitter() {
   Serial.print("Transmitting packet ... #"); Serial.println(sentNum);
   DW1000.newTransmit();
   DW1000.setDefaults();
-  String msg = "Hello DW1000, it's #"; msg += sentNum;
-  DW1000.setData(msg);
+  byte msg[6] = { 0, 2, 0, sentNum, 0, 0 }; //"Hello DW1000, it's #"; msg += sentNum;
+  DW1000.setData(msg, 6);
   // delay sending the message for the given amount
   DW1000Time deltaTime = DW1000Time(10, DW1000Time::MILLISECONDS);
   DW1000.setDelay(deltaTime);
@@ -89,41 +98,29 @@ void loop()
   if (!sentAck) {
     return;
   }
-  // continue on success confirmation
-  // (we are here after the given amount of send delay time has passed)
   sentAck = false;
-  // update and print some information about the sent message
-  Serial.print("ARDUINO delay sent [ms] ... "); Serial.println(millis() - delaySent);
-  DW1000Time newSentTime;
-  DW1000.getTransmitTimestamp(newSentTime);
-  Serial.print("Processed packet ... #"); Serial.println(sentNum);
-  Serial.print("Sent timestamp ... "); Serial.println(newSentTime.getAsMicroSeconds());
-  // note: delta is just for simple demo as not correct on system time counter wrap-around
-  Serial.print("DW1000 delta send time [ms] ... "); Serial.println((newSentTime.getAsMicroSeconds() - sentTime.getAsMicroSeconds()) * 1.0e-3);
-  sentTime = newSentTime;
   sentNum++;
-  // again, transmit some data
   transmitter();
-
 
   //******* RECEIVER LOOP ***********
   // enter on confirmation of ISR status change (successfully received)
   if (received) {
     numReceived++;
     // get data as string
-    DW1000.getData(message);
+    DW1000.getData(message, 6);
     Serial.print("Received message ... #"); Serial.println(numReceived);
-    Serial.print("Data is ... "); Serial.println(message);
-    Serial.print("FP power is [dBm] ... "); Serial.println(DW1000.getFirstPathPower());
-    Serial.print("RX power is [dBm] ... "); Serial.println(DW1000.getReceivePower());
-    Serial.print("Signal quality is ... "); Serial.println(DW1000.getReceiveQuality());
+    Serial.print("Data is ... "); //Serial.println(message);
+    for(int i = 0; i < 5; i++)
+    {
+      Serial.print(message[i]);
+    }
+    Serial.println(message[5]);
     received = false;
   }
   if (error) {
     Serial.println("Error receiving a message");
     error = false;
     DW1000.getData(message);
-    Serial.print("Error data is ... "); Serial.println(message);
   }
-  
+  delay(500);
 }
